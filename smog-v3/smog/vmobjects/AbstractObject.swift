c8f20416a8fbe98c6bda69e692eb0801d001c7b8
Kristofer <kris@youngers.org>
2023-08-17 14:47:39 -0400

    rough draft #1

---
+++ smog-v3/smog/vmobjects/AbstractObject.swift
@@ -0,0 +1,8 @@
+//
+//  AbstractObject.swift
+//  smog
+//
+//  Created by Kristofer Younger on 8/17/23.
+//
+
+import Foundation
---
+++ smog-v3/smog/vmobjects/SClass.swift
@@ -0,0 +1,8 @@
+//
+//  SClass.swift
+//  smog
+//
+//  Created by Kristofer Younger on 8/17/23.
+//
+
+import Foundation

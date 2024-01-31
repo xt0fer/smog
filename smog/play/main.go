13038e17109fda43627c72f23e4d73c7309a4a45
Kristofer <kris@youngers.org>
2024-01-31 08:29:37 -0500

    how to render object to native golanf type

--- smog/play/main.go
+++ smog/play/main.go
@@ -2,6 +2,12 @@ package main
 
 func main() {
 
+	t1 := NewVMInteger(4)
+	t2 := NewVMInteger(6)
+	op := NewMessage("+", t2)
+
+	result := t1.Send(op)
+	println(result.Value())
 }
 
 type VMObject struct {
@@ -22,6 +28,15 @@ func (o *VMObject) ClassOf() string {
 	return o.Kind
 }
 
+func (o *VMObject) Send(m *Message) *VMObject {
+	// TODO
+	return o
+}
+
+func (o *VMObject) Value() string {
+	return o.N
+}
+
 type ClassMap struct {
 	Classes map[string]*VMObject
 }
@@ -31,8 +46,22 @@ type VMInteger struct {
 	Value int32
 }
 
-func NewVMInteger(value int32) *VMInteger {
-	no := &VMInteger{Value: value}
+func NewVMInteger(value int32) *VMObject {
+	no := &VMObject{Fields: make([]*VMObject, 0)}
+	no.Fields = append(no.Fields, &VMObject{Kind: "Integer", N: value})
 	no.Kind = "Integer"
 	return no
+}
+
+type Message struct {
+	Selector string
+	Args     []*VMObject
+}
+
+func NewMessage(s string, args ...*VMObject) (m *Message) {
+	m = &Message{
+		Selector: s,
+	}
+	m.Args = args
+	return m
 }

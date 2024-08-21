4c4f94bc74bb2d2d88f165d09be2e453846688e6
Kristofer <kris@youngers.org>
2024-08-21 16:09:56 -0400

    adds a Czech thesis doc

--- src/internal/vm/Object.go
+++ src/internal/vm/Object.go
@@ -3,3 +3,13 @@ package vm
 type Object struct {
 	// this is a vm object
 }
+
+// SimpleHello = (
+// | name |
+// setName: aString (
+// name := aString 
+// )
+// printGreeting (
+// (’Hello, ’, name) print )
+// )
+
